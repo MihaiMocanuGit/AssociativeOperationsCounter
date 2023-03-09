28733104f1f422b0afda7026406f4f105fab8d68
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-03-09 14:33:03 +0200

    added timer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from validator import Validator
 import numpy as np
+import time
+start_time = time.time()
 
 def transform_array_into_matrix(np_array, cardinal: int):
     return np.reshape(np_array, (cardinal, cardinal), order='C')
@@ -7,11 +9,14 @@ def transform_array_into_matrix(np_array, cardinal: int):
 def main():
     n = int(input("What is the cardinal of the set?\n>>\t"))
     validator = Validator(n)
+
+    start_time = time.time()
     for operation in validator.start_generation():
         operation_matrix = transform_array_into_matrix(operation[0], n)
         counter = operation[1]
         print(operation_matrix)
         print((n-1)*"\t" + str(counter))
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 
 
