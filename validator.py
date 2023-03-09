89ca61d91f66f5dca1bc56e75cc1dc27937f6f6a
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-03-09 14:31:14 +0200

    deleted operation counting, it's not actually usefull

--- validator.py
+++ validator.py
@@ -9,9 +9,6 @@ class Validator:
         # used to generate all operations
         self.__generator = Generator(cardinal_set * cardinal_set, cardinal_set)
 
-        # computing the number of all existing operations. We will use this to count how many operations to be computed
-        # will be left
-        self.__no_operations = self.__compute_number_of_operations()
         self.__no_assoc_operations = 0
 
         # we will use this to generate all isomorphism from a generated semi-group
@@ -21,7 +18,7 @@ class Validator:
         # we can write x*(y*z) using elements from the set
         self.__cartesian_product = list(product(range(self.__cardinal), repeat=3))
     def __compute_number_of_operations(self):
-        return (self.__cardinal * self.__cardinal) ** self.__cardinal
+        return  self.__cardinal ** (self.__cardinal * self.__cardinal)
 
     def __is_associative(self) -> bool:
         # (x * y) * z =?= x * (y * z)
@@ -50,11 +47,9 @@ class Validator:
 
     def generate_next_valid(self):
         has_not_finished = self.__generator.generate_next()
-        self.__no_operations -= 1
 
         while has_not_finished and not self.__is_associative():
             has_not_finished = self.__generator.generate_next()
-            self.__no_operations -= 1
 
         return has_not_finished
 
