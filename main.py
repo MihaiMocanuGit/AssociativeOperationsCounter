32d062416c07cfb9c40bc5b6d33f8079fc23e5dc
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-03-09 14:34:22 +0200

    added timer

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from validator import Validator
+import time
 
 
 def main():
     n = int(input("What is the cardinal of the set?\n>>\t"))
     validator = Validator(n)
     counter = 0
+    start_time = time.time()
     while validator.generate_next_valid():
         print(validator.transform_array_into_matrix())
         counter += 1
         print((n-1)*"\t", counter)
 
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 if __name__ == '__main__':
     main()
